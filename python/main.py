c0f2fc2341567e869bf0f99dadcf803b269b8a50
Junha Park <96017487+junha2010@users.noreply.github.com>
2023-03-12 23:13:48 -0400

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,4 +1,9 @@
+from datetime import date
+
 command = ""
+is_cul = False
+
+
 
 while command.lower != "quit":
     command = input("> ")
@@ -6,7 +11,9 @@ while command.lower != "quit":
         print("""
         website - to show my website URL
         friends - to show my friends list
-        to do - to show to do list""")
+        to do - to show to do list
+        ID - to show my ID
+        date - to show todays date""")
     if command == "website":
         print("https://junha2010.github.io/ ctrl + click to open")
     if command == "friends":
@@ -16,3 +23,13 @@ while command.lower != "quit":
         complate Python and Pygame
         complate JavaScript
         """)
+    if command == "ID":
+        print("""birth year: 2010
+        phone: 1234-1234-1234
+        email: heavenly.junha@gmail.com, junhapark2010@gmail.com, park8325@wrdsb.ca, junhapark10@hotmail.com
+        family: MOM(soyoung), DAD(sungho), SISTER(juha), ME(junha)
+        """)
+    if command == "date":
+        today = date.today()
+        print("Today's date:", today)
+    
