c77c6a048931c5eebfc28b6aef83576d3bc3ad5c
Junha Park <96017487+junha2010@users.noreply.github.com>
2023-03-25 22:28:03 -0400

    dddd

---
+++ clicker/thirdclicker/app.js
@@ -0,0 +1,11 @@
+var score = 0;
+
+function add() {
+    score = score + 1;
+    update()
+}
+
+function update() {
+    document.getElementById("#number").value = score;
+    document.getElementById("#number").innerHTML.score;
+}
