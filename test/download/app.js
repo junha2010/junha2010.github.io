872c930f2334da91731678dece83b792ae21c813
Junha Park <96017487+junha2010@users.noreply.github.com>
2023-04-16 15:47:18 -0400

    d

---
+++ test/download/app.js
@@ -0,0 +1,8 @@
+$("#fileRequest").click(function(){ /* code to download? */ });function download(url) {
+    const a = document.createElement('a')
+    a.href = url
+    a.download = url.split('/').pop()
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+}
