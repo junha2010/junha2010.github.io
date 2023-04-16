cef5a73e741a84a95f2bb7cde91310728674f7c7
Junha Park <96017487+junha2010@users.noreply.github.com>
2023-04-16 16:16:59 -0400

    d

--- test/download/app.js
+++ test/download/app.js
@@ -1,9 +1,1 @@
 $("#fileRequest").click(function(){ /* code to download? */ });
-function download(url) {
-    const a = document.createElement('a')
-    a.href = url
-    a.download = url.split('/').pop()
-    document.body.appendChild(a)
-    a.click()
-    document.body.removeChild(a)
-}
